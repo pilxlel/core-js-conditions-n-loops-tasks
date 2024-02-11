a6e98bafb57e0c8bd0f4958b9692dcad826c0cd2
Andrew Homan <andrew@Andrews-MacBook-Pro.local>
2024-02-11 12:42:32 +0400

    feat: implement isPositive getMaxNumber isIsoscelesTriangle functions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,9 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  if (number >= 0) return true;
+  return false;
 }
 
 /**
@@ -38,8 +39,10 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a > b && a > c) return a;
+  if (b > a && b > c) return b;
+  return c;
 }
 
 /**
@@ -82,8 +85,11 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === b && a + b > c && a !== 0 && b !== 0 && c !== 0) return true;
+  if (a === c && a + c > b && a !== 0 && b !== 0 && c !== 0) return true;
+  if (b === c && b + c > a && a !== 0 && b !== 0 && c !== 0) return true;
+  return false;
 }
 
 /**
