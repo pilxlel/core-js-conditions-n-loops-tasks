ab2f53379f55f15f39ad33da3ce26b872ad7a209
Andrew Homan <andrew@Andrews-MacBook-Pro.local>
2024-02-12 18:16:28 +0400

    feat: implement few new functions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -448,8 +448,22 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const result = arr;
+  const n = result.length;
+
+  for (let i = 1; i < n; i += 1) {
+    const current = result[i];
+    let j = i - 1;
+
+    while (j >= 0 && result[j] > current) {
+      result[j + 1] = result[j];
+      j -= 1;
+    }
+    result[j + 1] = current;
+  }
+
+  return arr;
 }
 
 /**
